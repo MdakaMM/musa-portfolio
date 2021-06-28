72929d282b32e8e2b94e381a6a7aca04f587372d
Musa Mdaka <43600557+MdakaMM@users.noreply.github.com>
2021-06-28 21:54:32 +0200

    added to portfolio

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,36 +1,36 @@
 import css1 from '../img/portImages/cv_css.png';
 import css2 from '../img/portImages/ms.png';
 import react1 from '../img/portImages/react_styled.png';
-import img2 from '../img/portImages/maya-4.jpg';
-import img3 from '../img/portImages/maya-3.jpg';
+import img2 from '../img/portImages/maya-4.png';
+import img3 from '../img/portImages/maya-3.png';
 
 const portfolios = [
     {
         id: 1,
         category: 'CSS',
         image: css1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Resume',
-        text: 'Created with only HTML and CSS.'
+        // link1: 'https://www.google.com',
+        link2: 'https://lamanjoloholdings.co.za/index.php',
+        title: 'Lamanjolo Website',
+        text: 'Created with HTML, Bootstrap, CSS and PHP.'
     },
     {
         id: 2,
-        category: 'Javascript',
+        category: 'PHP',
         image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        // link1: 'https://www.google.com',
+        link2: 'https://www.igtech.co.za/timesheet/Admin-Full/auth-login.php',
+        title: 'TimeSheet Application',
+        text: 'Created with PHP'
     },
     {
         id: 3,
-        category: 'Animation',
+        category: 'Javascript',
         image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
+        // link1: 'https://www.google.com',
+        link2: 'https://igtech.co.za/thecore/',
+        title: 'The Core Website',
+        text: 'Created with HTML5 and CSS3'
     },
     {
         id: 4,
